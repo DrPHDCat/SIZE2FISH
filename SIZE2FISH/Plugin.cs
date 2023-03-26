d3401b345293db77006db0b8a13ccf345235479a
DrPHDCat <107643253+DrPHDCat@users.noreply.github.com>
2023-03-26 10:28:29 +0600

    Add files via upload

---
+++ SIZE2FISH/Plugin.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace SIZE2FISH
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+            Harmony harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+            harmony.PatchAll();
+        }
+    }
+    [HarmonyPatch(typeof(FishingHUD), "Start")]
+    static public class SIZE2FISH
+    {
+        [HarmonyPostfix]
+        static public void Postfix(FishingHUD __instance, ref GameObject ___fishSizeContainer)
+        {
+            ___fishSizeContainer.GetComponent<UnityEngine.UI.Text>().text = "SIZE: 2";
+        }
+    }
+}
---
+++ SIZE2FISH/obj/Debug/netstandard2.0/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace SIZE2FISH
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "SIZE2FISH";
+        public const string PLUGIN_NAME = "SIZE2FISH";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
